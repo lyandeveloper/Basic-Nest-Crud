ee57008a5790b1988065ee0694ecb303d14e3e5a
lyan_dev <elian.campos@topi.io>
2021-10-27 08:30:51 -0300

    feat: created basic crud

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), UsersModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
